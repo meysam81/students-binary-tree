e9aebcedacc6af3ef3fbedf075152cc6ff12d932
Meysam Azad <MeysamAzad81@yahoo.com>
2018-01-03 13:30:26 +0330

    initial binary tree of students

---
+++ binarytree.cpp
@@ -0,0 +1,7 @@
+#include "binarytree.h"
+
+binarytree::binarytree()
+{
+
+}
+
---
+++ binarytree.h
@@ -0,0 +1,11 @@
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+
+
+class binarytree
+{
+public:
+    binarytree();
+};
+
+#endif // BINARYTREE_H
