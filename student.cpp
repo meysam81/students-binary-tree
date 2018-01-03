976d0c0fee52ad1d05b915fe149e546653a8aed4
Meysam Azad <MeysamAzad81@yahoo.com>
2018-01-03 13:29:33 +0330

    add getters and setters and overloaded toString

--- student.cpp
+++ student.cpp
@@ -1,23 +1,67 @@
 #include "student.h"
 
-int student::getData() const
-{
-    return data;
-}
-
+// ============================================= comparison =================================================
 bool student::operator <(student secondStudent)
 {
-    return (this->getData() < secondStudent.getData());
+    return (this->getStudentID() < secondStudent.getStudentID());
+}
+// ============================================== toString ==================================================
+ostream& operator <<(ostream &output, student &std)
+{
+    output << "Student ID: " << std.getStudentID() << endl
+           << "First Name: " << std.getFirstName() << endl
+           << "Last Name: " << std.getLastName()<< endl
+           << "Current Grade: " << std.getGrade() << endl;
+    return output;
+}
+// =============================================== getters ==================================================
+int student::getStudentID() const
+{
+    return studentID;
 }
 
-student::student() : data(0), leftChild(nullptr), rightChild(nullptr)
+int student::getGrade() const
+{
+    return grade;
+}
+
+string student::getFirstName() const
+{
+    return firstName;
+}
+
+string student::getLastName() const
+{
+    return lastName;
+}
+
+student *student::getLeftChild() const
+{
+    return leftChild;
+}
+
+student *student::getRightChild() const
+{
+    return rightChild;
+}
+// =============================================== setters ==================================================
+void student::setRightChild(student *value)
+{
+    rightChild = value;
+}
+
+void student::setLeftChild(student *value)
+{
+    leftChild = value;
+}
+// ============================================ constructors ================================================
+student::student() : studentID(0), leftChild(nullptr), rightChild(nullptr)
 {
 
 }
 
-student::student(int ID, student *leftChild, student *rightChild)
-    : data(ID), leftChild(leftChild), rightChild(rightChild)
+student::student(int ID, int grade, string firstName, string lastName, student *leftChild, student *rightChild)
+    : studentID(ID), grade(grade), firstName(firstName), lastName(lastName), leftChild(leftChild), rightChild(rightChild)
 {
 
 }
-
--- student.h
+++ student.h
@@ -1,20 +1,47 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+#include <iostream>
+#include <string>
+using namespace std;
 
 class student
 {
 private:
-    int data;
-    student* leftChild,
-        *rightChild;
+    // student class containing 3 fields as follow
+    int studentID;
+    int grade;
+    string firstName;
+    string lastName;
+    student *leftChild;
+    student *rightChild;
+
 public:
+    // constructor
     student();
-    student(int ID, student* leftChild, student* rightChild);
+    student(int ID, int grade,
+            string firstName, string lastName,
+            student* leftChild, student* rightChild);
 
-    int getData() const;
+    // getters
+    int getStudentID() const;
+    int getGrade() const;
+    student *getRightChild() const;
+    student *getLeftChild() const;
+    string getFirstName() const;
+    string getLastName() const;
 
+    // setters
+    void setRightChild(student *value);
+    void setLeftChild(student *value);
+
+    // comparison operator overloading
     bool operator < (student secondStudent);
+
+    // just a declaration of toString method
+    friend ostream &operator <<(ostream& output, student& std);
 };
+
+ostream& operator <<(ostream &output, student &std);
 
 #endif // STUDENT_H
