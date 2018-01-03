b559b3a1626ad195cb870c05542b0f83b741d289
Meysam Azad <MeysamAzad81@yahoo.com>
2018-01-03 12:26:39 +0330

    student constructor

--- student.cpp
+++ student.cpp
@@ -1,6 +1,12 @@
 #include "student.h"
 
-student::student()
+student::student() : data(0), leftChild(nullptr), rightChild(nullptr)
+{
+
+}
+
+student::student(int ID, student *leftChild, student *rightChild)
+    : data(ID), leftChild(leftChild), rightChild(rightChild)
 {
 
 }
--- student.h
+++ student.h
@@ -4,8 +4,13 @@
 
 class student
 {
+private:
+    int data;
+    student* leftChild,
+        *rightChild;
 public:
     student();
+    student(int ID, student* leftChild, student* rightChild);
 };
 
 #endif // STUDENT_H
