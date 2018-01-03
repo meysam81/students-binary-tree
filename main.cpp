377e23031629a6b370055824825b185753524ba8
Meysam Azad <MeysamAzad81@yahoo.com>
2018-01-03 12:14:36 +0330

    Still initial stage

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
-#include <QCoreApplication>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
 
-    return a.exec();
+    return 0;
 }
-
--- students-binary-tree.pro
+++ students-binary-tree.pro
@@ -2,7 +2,7 @@ QT += core
 QT -= gui
 
 TARGET = students-binary-tree
-CONFIG += console
+CONFIG += console c++11
 CONFIG -= app_bundle
 
 TEMPLATE = app
